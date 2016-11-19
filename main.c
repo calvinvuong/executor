809e8d0d74bccd2fe5f21a1de47bf5f7a8b561c9
Calvin Vuong <cvuong@stuy.edu>
2016-11-18 19:03:03 -0500

    rewrote lost files

---
+++ main.c
@@ -0,0 +1,32 @@
+// Calvin Vuong & William Xiang
+// MKS65 pd10
+// Work 07
+// 11-18-2016
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+
+  printf("Command: ");
+  char input[1000];
+  fgets(input, sizeof(input), stdin);
+  input[ strlen(input) - 1 ] = '\0'; // get rid of newline
+
+  char * command[1000];
+  char *s = input;
+
+  // split the input by argument
+  int i = 0;
+  while ( s != NULL ) {
+    command[i] = strsep(&s, " ");
+    i++;
+  }
+  command[i] = NULL; // null terminate
+
+  execvp( command[0], command );
+
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+compile: main.c
+	gcc main.c
+
+run: compile
+	./a.out
